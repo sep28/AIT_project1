b2ca4ce2317b9f6baf7569124faaddef05346a59
Sep <sepmacbook@10-16-139-197.dynapool.wireless.nyu.edu>
2019-09-15 18:42:26 -0400

    saving changes to module file

--- src/connectmoji.js
+++ src/connectmoji.js
@@ -1,2 +1,28 @@
 // implement your functions here
 // ...don't forget to export functions!
+
+function generateBoard(rows, cols, fill = null) {
+	
+	const B = {
+		data: new Array((rows*cols)),
+		rows: rows,
+		cols: cols
+	};
+	B.data.fill(fill);
+
+	//let arraySize = rows * cols;
+	//for (let i = 0; i < arraySize; i++) {
+		//B.data.push(fill);
+	//}
+	return B;
+}
+
+function rowColToIndex(board, row, col) {
+	
+	let totalCols = board.cols; 
+	const idx = (totalCols * row) + col;
+	return idx;
+}
+
+
+		
